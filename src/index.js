d36c3ea09e73f35d297f3ca3c3d764d763046400
Julia Barcelos <julaai@gmail.com>
2022-01-10 15:23:42 -0300

    create combine reducers on index from reducers.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
@@ -7,9 +8,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-  < Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
