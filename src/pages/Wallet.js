60ada21ba08c4098d9897fb876004f71b6e9e6a1
Julia Barcelos <julaai@gmail.com>
2022-01-11 15:11:02 -0300

    made comunication between action, reducer,component and need fix communication with store.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import SpendingForm from '../components/SpendingForm';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <SpendingForm />
+      </>
     );
   }
 }
--- src/tests/wallet.test.js
+++ src/tests/wallet.test.js
@@ -38,7 +38,7 @@ describe('2 - Crie uma página para sua carteira com as seguintes característic
   });
 });
 
-describe.only('3 - Crie um header para a página de carteira contendo as seguintes características:', () => {
+describe('3 - Crie um header para a página de carteira contendo as seguintes características:', () => {
   const initial = initialStateHeader;
 
   test('Um elemento que exiba o email do usuário que fez login.', () => {
@@ -66,7 +66,7 @@ describe.only('3 - Crie um header para a página de carteira contendo as seguint
   });
 });
 
-describe('4 - Desenvolva um formulário para adicionar uma despesa contendo as seguintes características:', () => {
+describe.only('4 - Desenvolva um formulário para adicionar uma despesa contendo as seguintes características:', () => {
   test('Um campo para adicionar o valor da despesa', async () => {
     renderWithRouterAndStore(<Wallet />, '/carteira');
     const valueInput = await screen.findByTestId(VALUE_INPUT_TEST_ID);
