c522f114b20a0978987728191b39a0939188abb3
Julia Barcelos <julaai@gmail.com>
2022-01-12 12:16:03 -0300

    render Form

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
-import SpendingForm from '../components/SpendingForm';
+import SpendForm from '../components/SpendForm';
 
 class Wallet extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <SpendingForm />
+        <SpendForm />
       </>
     );
   }
